a3e6f21b81a8d54b3ff5e38aa8aaa851dd738fe4
priyakalot <30380605+priyakalot@users.noreply.github.com>
2018-06-22 11:55:04 +0800

    add comments

--- data.py
+++ data.py
@@ -3,32 +3,41 @@ from midi_to_statematrix import upperBound, lowerBound
 
 def startSentinel():
     def noteSentinel(note):
+        # input number for note in range of lowerBound to upperBound 
         position = note
         part_position = [position]
         
-        pitchclass = (note + lowerBound) % 12
-        part_pitchclass = [int(i == pitchclass) for i in range(12)]
+        pitchclass = (note + lowerBound) % 12 # 12 notes in an octave including naturals and sharps/flats
+        part_pitchclass = [int(i == pitchclass) for i in range(12)] 
         
-        return part_position + part_pitchclass + [0]*66 + [1] 
-    return [noteSentinel(note) for note in range(upperBound-lowerBound)]
+        return part_position + part_pitchclass + [0]*66 + [1] # length will be 80, figure out why
+    return [noteSentinel(note) for note in range(upperBound-lowerBound)] 
 
+# l is state, i is a value to move up or down from current note
 def getOrDefault(l, i, d):
     try:
         return l[i]
     except IndexError:
         return d
 
+# buildContext notes:
+# counts how many of each pitch class in the file
+# state is a list of 1x2 lists, list of notestates, stores "memory" of previous note 
+# notestate is a 1x2 list that indicates whether the previous note was played or articulated
+
 def buildContext(state):
-    context = [0]*12
+    context = [0]*12 # initialize octave values with all 0
     for note, notestate in enumerate(state):
         if notestate[0] == 1:
             pitchclass = (note + lowerBound) % 12
             context[pitchclass] += 1
     return context
-    
+
+# determines value for each type of note (half notes, quarter notes, etc) based on time (tempo)
 def buildBeat(time):
     return [2*x-1 for x in [time%2, (time//2)%2, (time//4)%2, (time//8)%2]]
 
+# beat is output of buildBeat
 def noteInputForm(note, state, context, beat):
     position = note
     part_position = [position]
